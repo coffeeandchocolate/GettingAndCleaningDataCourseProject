a44905deb1b6f1c30c29afb939648a3119dd0d0d
coffeeandchocolate <ykao@wested.org>
2015-03-08 23:19:56 -0700

    Committing skeletons of all required files for assignment

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# GettingAndCleaningDataCourseProject
+# Getting and Cleaning Data Course Project: README
+**By:** coffeeandchocolate
+
+## How It Works
+There is only one script in this project:
+* run_analysis.R
+
+Running this script will download the zip file that contains the data
+for this project, unzip it, and complete all the steps of the assignment
+
+## Assumptions
+1. Your R working directory is already set to the folder where you want the data to be downloaded
---
+++ run_analysis.R
@@ -0,0 +1,25 @@
+# Getting and Cleaning Data
+# Course Project
+# By: coffeeandchocolate
+
+dataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+# STEP 1
+# Merge the training and the test sets to create one data set.
+
+
+# STEP 2
+# Extract only the measurements on the mean and standard deviation for each measurement. 
+
+
+# STEP 3
+# Use descriptive activity names to name the activities in the data set
+
+
+# STEP 4
+# Appropriately label the data set with descriptive variable names. 
+
+
+# STEP 5
+# From the data set in step 4, creates a second, independent tidy data set with the 
+# average of each variable for each activity and each subject.
